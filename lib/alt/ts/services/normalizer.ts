47df017edb463ae41b61ce67917116d2bc529808
mmmpa <mmmpa.mmmpa@gmail.com>
2016-02-03 14:58:46 +0900

    updated

--- lib/alt/ts/services/normalizer.ts
+++ lib/alt/ts/services/normalizer.ts
@@ -1,40 +1,10 @@
 import Constants from "../initializers/constants";
 import * as _ from 'lodash';
 
-export function normalizePieData(props) {
-  let {data, split, table} = props;
-
-  switch (split) {
-    case undefined:
-      return normalizePieDataYear(props);
-    case 'area':
-      return normalizePieDataNormal(data, split, table);
-    case 'gender':
-      return normalizePieDataNormal(data, split, table);
-    default:
-      return normalizePieDataReverse(data, table, split);
-  }
-}
-
-export function normalizeBarData(props) {
-  let {data, split, table} = props;
-
-  switch (split) {
-    case undefined:
-      return normalizePieDataYear(props);
-    case 'area':
-      return normalizeBarDataNormal(data, split, table);
-    case 'gender':
-      return normalizeBarDataNormal(data, split, table);
-    default:
-      return normalizeBarDataReverse(data, table, split);
-  }
-}
-
 export function normalizeStackBarData(props) {
   let {data, split, table, rotation, sort} = props;
   let arranged = arrangeData(data, table);
-  console.log(arranged)
+
   if (rotation == 'true') {
     return normalizeRotatedStackBarData(arranged, table, split, sort);
   } else {
@@ -106,13 +76,93 @@ function arrangeData(data:any[], table:string):any {
 }
 
 function normalizeRotatedStackBarData(arranged, table, split, sort) {
-  console.log(arranged)
+  console.log('rotated');
+  console.log(arranged);
+
+  let keyMaps = detectTableKeyMap(table);
+  let splitterMaps = detectSplitterMap(split);
+  let sortMaps = detectSplitterMap(sort);
+
+  let chartSeries = keyMaps.map((keyMap)=> ({field: keyMap.key, name: keyMap.name}));
+
+  let chartData = {
+    chartSeries,
+    dataList: {},
+    max: 0
+  };
+
+  if (sort == 'year') {
+  } else {
+    _.each(arranged.year, (yearDataList, year:string)=> {
+      let eachYearStore = findOrCreate(chartData.dataList, year, {});
+      eachYearStore.title = yearDataList[0].year.name;
+      let remap = {};
+      _.each(yearDataList, (d)=> {
+        let store = findOrCreate(remap, d[sort].content, {})
+        store[d[split].content] = d;
+      });
+
+      console.log('1', chartData)
+
+      _.each(sortMaps, (sortMap)=> {
+        let sortElement = remap[sortMap.key];
+        if (!sortElement) {
+          return;
+        }
+        _.each(sortElement, (splitValue, splitKey:string)=> {
+          // splitごとのchart全体のデータの入れ物
+          let dataList = findOrCreate(eachYearStore, splitKey, {});
+          //sortの順番にしたがって、チャートシリーズが全て入ったobjectを挿入する
+          _.each(keyMaps, (keyMap)=> {
+            let data = findOrCreate(dataList, sortMap.key, {sort: sortMap});
+            data[keyMap.key] = splitValue[keyMap.key].content;
+          });
+        });
+      });
+      console.log('eachYearStore', eachYearStore)
+
+
+      _.each(splitterMaps, (spMap)=> {
+        let store = eachYearStore[spMap.key];
+        var chartList = findOrCreate(eachYearStore, 'chartList', []);
+        let array = [];
+        _.each(sortMaps, (sortMap)=> {
+          array.push(store[sortMap.key]);
+        });
+        chartList.push({
+          key: spMap.key,
+          name: spMap.name,
+          data: array
+        });
+        //delete eachYearStore[keyMap.key];
+      });
+    });
+
+    // max処理
+    _.each(chartData.dataList, (dataSet)=> {
+      _.each(keyMaps, (keyMap)=> {
+        let tableData = dataSet[keyMap.key];
+        _.each(tableData, (data)=> {
+          let maxStore = 0;
+          _.each(splitterMaps, (sp)=> {
+            maxStore += data[sp.key] || 0;
+          });
+          maxStore > chartData.max && (chartData.max = maxStore);
+        })
+      });
+    });
+
+  }
+
+  console.log('normalized', chartData);
+  return chartData;
 }
 
 function normalizeRegularStackBarData(arranged, table, split, sort) {
   if (!_.isObject(arranged)) {
     return [];
   }
+
   let keyMaps = detectTableKeyMap(table);
   let splitterMaps = detectSplitterMap(split);
   let sortMaps = detectSplitterMap(sort);
@@ -126,21 +176,6 @@ function normalizeRegularStackBarData(arranged, table, split, sort) {
   };
 
   if (sort == 'year') {
-    let sample = {
-      dataList: {
-        column: {
-          title: 'タイトル',
-          chartList: [
-            {
-              key: 'columnName',
-              name: 'columnTitle',
-              data: [{}]
-            }
-          ]
-        }
-      }
-    };
-
     chartData.dataList = {
       _: {
         title: '年ごとの遷移',
@@ -149,7 +184,7 @@ function normalizeRegularStackBarData(arranged, table, split, sort) {
     };
     let store = chartData.dataList._.chartList;
     _.each(arranged[split], (splitData, key)=> {
-      _.each(splitData, (spData)=>{
+      _.each(splitData, (spData)=> {
         _.each(keyMaps, (keyMap)=> {
           let columnStore = findOrCreate(store, keyMap.key, {name: keyMap.name, data: {}});
           let yearStore = findOrCreate(columnStore.data, spData.year.content, {sort: spData.year});
@@ -159,7 +194,7 @@ function normalizeRegularStackBarData(arranged, table, split, sort) {
     });
     _.each(keyMaps, (keyMap)=> {
       let target = store[keyMap.key];
-      target.data = _.map(target.data, (value)=>{
+      target.data = _.map(target.data, (value)=> {
         let maxStore = 0;
         _.each(splitterMaps, (sp)=> {
           maxStore += value[sp.key] || 0;
@@ -168,7 +203,6 @@ function normalizeRegularStackBarData(arranged, table, split, sort) {
         return value;
       });
     })
-
   } else {
     _.each(arranged.year, (yearDataList, year:string)=> {
       let eachYearStore = findOrCreate(chartData.dataList, year, {});
@@ -224,371 +258,6 @@ function normalizeRegularStackBarData(arranged, table, split, sort) {
     });
   }
 
-
   console.log('normalized', chartData);
   return chartData;
-}
-
-function sortData(dataList, split) {
-  if (!dataList) {
-    return [];
-  }
-
-  switch (split) {
-    case 'year':
-      return _.sortBy(dataList, (el)=> -el.year.content);
-    case 'gender':
-      return _.sortBy(dataList, (el)=> el.gender.content);
-    case 'area':
-      return _.sortBy(dataList, (el)=> el.area.content);
-    default:
-      return dataList;
-  }
-}
-
-
-function detectChartProps(table) {
-  return Constants[`${table}Props`] || {};
-}
-
-
-function splitYear(dataList:any[]) {
-  let result = {};
-
-  _.map(dataList, (data)=> {
-    let year = data.year.content;
-    if (!result[year]) {
-      result[year] = {
-        year: data.year,
-        dataList: []
-      }
-    }
-    result[year].dataList.push(data);
-  });
-
-  return result;
-}
-
-function posit(dataList:any[], split:string, keys:string[]) {
-  let result = {};
-
-  dataList.map((data)=> {
-    keys.map((key)=> {
-      if (!result[key]) {
-        result[key] = {}
-      }
-      let sortKey = data[split].content;
-      result[key][sortKey] = data[key];
-    });
-  });
-
-  return result;
-}
-
-function positReverse(dataList:any[], split:string, keys:string[]) {
-  let result = {};
-
-  dataList.map((data)=> {
-    keys.map((key)=> {
-      let sortKey = data[split].content;
-      if (!result[sortKey]) {
-        result[sortKey] = {}
-      }
-      result[sortKey][key] = data[key];
-    });
-  });
-  return result;
-}
-
-function normalizePieDataReverse(data:any[], split:string, table:string) {
-  console.log('reverse');
-  let sorted = sortData(data, split);
-  let {keys, texts} = detectChartProps(table);
-
-  if (!keys || !texts) {
-    return [];
-  }
-
-  let years = splitYear(sorted);
-  let elements = Constants[`${split}s`];
-
-  let normalizedList = {};
-
-  _.forEach(years, (value, key)=> {
-    let result = positReverse(value.dataList, split, keys);
-    result.year = value.year;
-    normalizedList[key] = result;
-  });
-
-  let results = [];
-
-  _.forEach(normalizedList, (normalized, key)=> {
-    let result = {
-      year: normalized.year,
-      dataList: []
-    };
-
-    _.forEach(elements, (el)=> {
-      let data = normalized[el.key]
-
-      let total = _.reduce(keys, (a, ek)=> {
-        return a + data[ek]
-      }, 0);
-
-      result.dataList.push({
-        name: `${el.text} (${total})`,
-        data: _.sortBy(_.compact(_.zip(keys, texts).map((kt)=> {
-          let key = kt[0];
-          let title = kt[1];
-          let label = `${title} (${data[key]})`;
-          return {label, value: par(data[key], total)}
-        })), (d)=> 0)
-      })
-    });
-    results.push(result);
-  });
-
-  return _.sortBy(results, (result)=> -result.year.content);
-}
-
-function normalizePieDataNormal(data:any[], split:string, table:string) {
-  let sorted = sortData(data, split);
-  let {keys, texts} = detectChartProps(table);
-
-  if (!keys || !texts) {
-    return [];
-  }
-
-  let years = splitYear(sorted);
-  let elements = Constants[`${split}s`];
-
-  let normalizedList = {};
-
-  _.forEach(years, (value, key)=> {
-    let result = posit(value.dataList, split, keys);
-    result.year = value.year;
-    normalizedList[key] = result;
-  });
-
-  let results = [];
-
-  _.forEach(normalizedList, (normalized, key)=> {
-    let result = {
-      year: normalized.year,
-      dataList: []
-    };
-    _.forEach(_.zip(keys, texts), (kt)=> {
-      let key = kt[0];
-      let title = kt[1];
-
-      let data = normalized[key];
-      let elementKeys = elements.map((e)=> e.key);
-
-      let total = _.reduce(elementKeys, (a, ek)=> {
-        return a + data[ek]
-      }, 0);
-
-      result.dataList.push({
-        name: `${title} (${total})`,
-        data: _.compact(elements.map((el)=> {
-          if (data[el.key] == 0) {
-            return null;
-          }
-          let label = `${el.text} (${data[el.key]})`
-          return {label, value: par(data[el.key], total)}
-        }))
-      })
-    });
-    results.push(result);
-  });
-
-  return _.sortBy(results, (result)=> -result.year.content);
-}
-
-
-function normalizeBarDataNormal(data:any[], split:string, table:string) {
-  console.log('regular')
-  let sorted = sortData(data, split);
-  let {keys, texts} = detectChartProps(table);
-
-  if (!keys || !texts) {
-    return [];
-  }
-
-  let years = splitYear(sorted);
-  let elements = Constants[`${split}s`];
-
-  console.log('years', years)
-
-  let normalizedList = {};
-
-  _.forEach(years, (value, key)=> {
-    let result = posit(value.dataList, split, keys);
-    result.year = value.year;
-    normalizedList[key] = result;
-  });
-
-  let chartSeries = elements.map((e)=> ({field: e.key, name: e.text}));
-
-  let max = 0;
-
-  let result = {};
-  _.forEach(normalizedList, (normalized, key)=> {
-    _.forEach(_.zip(keys, texts), (kt)=> {
-
-      let key = kt[0];
-      let title = kt[1];
-
-      let data = normalized[key];
-
-      let now = {
-        year: normalized.year.content
-      };
-      let myNum = 0;
-      elements.map((e)=> {
-        let num = data[e.key];
-        if (num != 0 && !num) {
-          _.remove(chartSeries, (c)=> c.field == e.key)
-          return;
-        }
-        myNum += num;
-
-        now[e.key] = num
-      });
-      myNum > max && (max = myNum);
-
-      if (!result[key]) {
-        result[key] = [];
-      }
-      result[key].push(now);
-    });
-  });
-  console.log('now', result)
-
-  let results = []
-  _.forEach(_.zip(keys, texts), (kt)=> {
-
-    let key = kt[0];
-    let title = kt[1];
-
-    results.push({
-      chartSeries,
-      key: key,
-      title: title,
-      dataList: result[key]
-    });
-  });
-
-  return {max, results};
-}
-
-function normalizeBarDataReverse(data:any[], split:string, table:string) {
-  console.log('rotated');
-  let sorted = sortData(data, split);
-  let {keys, texts} = detectChartProps(table);
-
-  if (!keys || !texts) {
-    return [];
-  }
-
-  let years = splitYear(sorted);
-  let elements = Constants[`${split}s`];
-
-
-  let normalizedList = {};
-
-  _.forEach(years, (value, key)=> {
-    let result = posit(value.dataList, split, keys);
-    result.year = value.year;
-    normalizedList[key] = result;
-  });
-
-  let chartSeries = _.zip(keys, texts).map((kt)=> ({field: kt[0], name: kt[1]}));
-  console.log('chartSeries', chartSeries)
-
-  let result = {};
-  _.forEach(normalizedList, (normalized, key)=> {
-    let store = {};
-    _.forEach(_.zip(keys, texts), (kt)=> {
-      let key = kt[0];
-      let title = kt[1];
-
-      elements.map((e)=> {
-        if (!store[e.key]) {
-          store[e.key] = {year: normalized.year.content}
-        }
-        let num = normalized[key][e.key];
-        store[e.key][key] = num;
-      });
-    });
-    elements.map((e)=> {
-      if (!result[e.key]) {
-        result[e.key] = [];
-      }
-      result[e.key].push(store[e.key]);
-    });
-  });
-
-  console.log('now', result)
-  let max = 0;
-
-  let results = []
-  _.forEach(elements, (e)=> {
-    if (!result[e.key][0][keys[0]]) {
-      return;
-    }
-    results.push({
-      chartSeries,
-      key: e.key,
-      title: e.text,
-      dataList: result[e.key]
-    });
-
-    result[e.key].map((d)=> {
-      var myNum = 0;
-      keys.map((k)=> {
-        myNum += d[k];
-      });
-      myNum > max && (max = myNum);
-    });
-  });
-  console.log('results', results)
-
-  return {max, results};
-}
-
-function detectYearTable(table) {
-  return table == 'gender' || table == 'area' ? 'total' : table;
-}
-
-function normalizePieDataYear(props) {
-  let {data, table} = props;
-  let sorted = sortData(data, 'year');
-  let {keys, texts} = detectChartProps(detectYearTable(table));
-
-  console.log(keys, texts, sorted)
-
-  if (!keys || !texts) {
-    return [];
-  }
-
-  return sorted.map((one)=> {
-    let total = _.reduce(keys, (a, key)=>  a + one[key], 0);
-
-    return {
-      year: one.year,
-      dataList: [{
-        name: `(${total})`,
-        data: _.map(_.zip(keys, texts), (kt)=> {
-          let key = kt[0];
-          let label = `${kt[1]} (${one[key]})`;
-          return {label, value: par(one[key], total)}
-        })
-      }]
-    }
-  });
-}
-
-function par(n, total) {
-  return Math.round(n / total * 1000) / 10
 }
