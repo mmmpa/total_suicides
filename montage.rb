15d3c55f1153b7d6605576e9cd374c4a08ad53a0
mmmpa <mmmpa.mmmpa@gmail.com>
2016-02-21 05:15:36 +0900

    updated

--- montage.rb
+++ montage.rb
@@ -1,6 +1,6 @@
 files = Dir['./log/ss/**/*.png']
 file_names = files.join(' ')
-w = 4
-h = (files.size.to_f / w) + 1
+w = 20
+h = (files.size / w.to_f).ceil
 
-`montage -background black -tile #{w}x#{h} -geometry 150x100 #{file_names} montagged.png`
+`montage -background black -tile #{w}x#{h} -geometry 50x30 #{file_names} montagged.png`
--- spec/features/chart_spec.rb
+++ spec/features/chart_spec.rb
@@ -2,7 +2,45 @@ require 'capybara_helper'
 
 feature 'チャートページ' do
   before :each do |ex|
-    ready_ss(ex)
+    ready_ss(ex, 800)
+  end
+
+  scenario '全てのチャート' do
+    visit '/'
+
+    queries = [
+      '.data-selector.x input',
+      '.data-selector.y input',
+      '.data-selector.y-specifier input',
+      '.data-selector.z-specifier input'
+    ]
+    combinations = []
+
+    all(queries[0]).each do |x|
+      x.click
+      all(queries[1]).each do |y|
+        y.click
+        all(queries[2])[0..3].each do |y_specifier|
+          y_specifier.click
+          z = all(queries[3])
+          if z.size != 0
+            combinations.push [x.value, y.value, y_specifier.value, z.first.value]
+          else
+            combinations.push [x.value, y.value, y_specifier.value]
+          end
+        end
+      end
+    end
+
+    combinations.each do |combination|
+      visit '/'
+      combination.zip(queries) do |value, query|
+        find(query + %{[value="#{value}"]}).click if value.present?
+      end
+      find('button.submit').click
+      sleep 0.5
+      take_ss('チャートを表示')
+    end
   end
 
   feature '横軸が年のチャート' do
